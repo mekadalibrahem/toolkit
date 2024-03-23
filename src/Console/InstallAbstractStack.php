7f9d2aa0fe513ea4d93f51f516ee12a5cb024ad9
MekadAlIibrahem <mekadalibrahem123@gmail.com>
2024-03-23 21:56:33 +0300

    add copu requests files in install abastract

--- src/Console/InstallAbstractStack.php
+++ src/Console/InstallAbstractStack.php
@@ -11,8 +11,13 @@ trait InstallAbstractStack
      */
     public function installAbstarctStack()
     {
-         // Controllers...
-         (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers'));
-         (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/default/app/Http/Controllers', app_path('Http/Controllers'));
+
+        // Requests
+        (new Filesystem)->ensureDirectoryExists(app_path('Http/Requests'));
+        (new Filesystem)->copyDirectory(__DIR__ ."/../../stubs/abstract/app/Http/Requests",app_path('Http/Requests'));
+
+        // Controllers...
+        (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/abstract/app/Http/Controllers', app_path('Http/Controllers'));
     }
 }
