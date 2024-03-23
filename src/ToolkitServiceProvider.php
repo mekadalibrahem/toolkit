e1788dfe527d5e4795b43ce3bdd028bb6c5b3de7
MekadAlIibrahem <mekadalibrahem123@gmail.com>
2024-03-23 20:43:53 +0300

    init service provider for package

---
+++ src/ToolkitServiceProvider.php
@@ -0,0 +1,21 @@
+<?php 
+namespace Mekadalibrahem\Toolkit ;
+
+use Illuminate\Support\ServiceProvider ;
+
+class ToolkitServiceProvider extends ServiceProvider 
+{
+    
+    public function register()
+    {
+        // 
+    }
+
+
+    public function boot()
+    {
+        //
+    }
+
+    
+}
