09dde72cb99259493d9670c96f95da082b70ea00
MekadAlIibrahem <mekadalibrahem123@gmail.com>
2024-03-23 22:24:47 +0300

    fix handle method name in InstallCommand:class  and add prompt for missing arguments  - fix path for auth Controller

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -5,22 +5,43 @@ namespace Mekadalibrahem\Toolkit\Console ;
 use Illuminate\Console\Command ;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 
+use function Laravel\Prompts\select;
+
 class InstallCommand extends Command  implements PromptsForMissingInput
 {
     use InstallAbstractStack ;
 
 
-    protected $signature = "toolkit:install {stack : The development stack that should be installed (abstarct)}" ;
+    protected $signature = "toolkit:install {stack : The development stack that should be installed (abstract)}" ;
 
     protected $description = "Install the Tool kit package" ;
 
-    public function handel() 
+    public function handle() 
     {
-        if ($this->argument('stack') === 'abstarct') {
+        if ($this->argument('stack') === 'abstract') {
             return $this->installAbstarctStack() ;
         }
-        $this->components->error('Invalid stack. Supported stacks are [abstarct]');
+        $this->components->error('Invalid stack. Supported stacks are [abstract]');
 
         return 1;
     }
+          /**
+     * Prompt for missing input arguments using the returned questions.
+     *
+     * @return array
+     */
+    protected function promptForMissingArgumentsUsing()
+    {
+        return [
+            'stack' => fn () => select(
+                label: 'Which toolkit stack would you like to install?',
+                options: [
+                    'abstract' => 'with  out view or GUI'
+
+                ],
+                scroll: 1,
+            ),
+        ];
+    }
+
 }
