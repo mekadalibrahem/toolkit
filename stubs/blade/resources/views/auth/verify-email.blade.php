031749b24c80df75552a938706e16b871ff6f296
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-03-26 21:50:37 +0300

    fix bug not suporte Post method in route logout

--- stubs/blade/resources/views/auth/verify-email.blade.php
+++ stubs/blade/resources/views/auth/verify-email.blade.php
@@ -2,34 +2,42 @@
     <x-slot:title >
        Register
     </x-slot:title>
+    <section class="">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div class="w-full lg:p-10 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <div class="">
+                        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                    </div>
+                    @if (session('status') == 'verification-link-sent')
+                        <div class="p-1  text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+                            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                        </div>
+                    @endif
+                    <div class="mt-4 flex items-center justify-between">
+                        <form method="POST" action="{{ route('verification.send') }}">
+                            @csrf
 
-<div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-    {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-</div>
+                            <div>
+                                <x-button.primary>
+                                    {{ __('Resend Verification Email') }}
+                                </x-button.primary>
+                            </div>
+                        </form>
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
-
-        <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
-
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+                        <form action="{{Route('logout_handler')}}" method="GET">
+                            @csrf
+                            <x-button.danger type="submit" >logout</x-button.danger>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </form>
 
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
+        </div>
 
-            <button type="submit" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                {{ __('Log Out') }}
-            </button>
-        </form>
-    </div>
+    </section>
+
+
+
+
 </x-layouts.app >
