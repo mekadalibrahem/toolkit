6ccd61ee538127ff3bc3919d243a66ed7124a0fb
mekadalibrahem <mekadalibrahem123@gmail.com>
2024-03-27 01:57:29 +0300

    fix bugs

--- stubs/blade/app/Http/Controllers/ProfileController.php
+++ stubs/blade/app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
--- stubs/blade/routes/web.php
+++ stubs/blade/routes/web.php
@@ -2,23 +2,9 @@
 
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::middleware([
-    'auth' , 'verified'
-])->group(function(){
-
-    Route::name('profile.')->group(function(){
-        Route::get('/profile' , [ProfileController::class , 'create'])->name('create');
-        Route::post('/profile' , [ProfileController::class , 'update'])->name('update');
-        Route::delete('/profile' , [ ProfileController::class , 'destroy' ])->name('destroy');
-        Route::post('/change-password' , [ProfileController::class , 'change_password'])->name('change-password');
-    });
-});
 require __DIR__.'/web/auth.php';
